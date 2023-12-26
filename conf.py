f8510bce0985fdd4dd6da35901b1f6fde45aba06
Rainer Thieringer <rainerthi@gmail.com>
2023-12-26 11:48:14 +0100

    Farbiges Wappen.

--- conf.py
+++ conf.py
@@ -126,7 +126,7 @@ html_theme = 'sphinx_book_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-html_logo = '_static/wappen_thieringer.png'
+html_logo = '_static/wappen_thieringer_farbe.gif'
 # html_favicon = "_static/trumpf_logo.ico"
 
 # These paths are either relative to html_static_path
@@ -305,7 +305,7 @@ if docscope == 'internal':
 	releases_debug = True
 
 	exclude_patterns = [
-		'_build', 'Thumbs.db', '.DS_Store', '_input', 'node_modules', 'build-helpers', '**/nnnn', '**.inc.rst', 'README.md'
+		'_build', 'Thumbs.db', '.DS_Store', '_input', 'node_modules', 'build-helpers', '**/nnnn', '**.inc.rst', 'README*.md'
 	]
 	todo_include_todos = True
 
